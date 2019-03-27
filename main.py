8b1f33aeae34f3a5a249639f6e0228d8ae011919
dogasp <dorian.gaspar95@free.fr>
2019-03-27 22:48:28 +0100

    miniature

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from Reseau.client import *
 #import Tete_chercheuse.tete_chercheuse
 class BoutonS:
     def __init__(self, x, y, jeux, run):
-        self.x = x
-        self.y = y
+        #self.bouton = Button(img = )
+        self.image = PhotoImage(file = "thumbnail/" + jeux + ".jpg")
+        self.button = Button(Frame_main, image = self.image)
+        self.button.grid(row = x, column = y)
         self.jeux = jeux
         self.run = run
 
@@ -37,9 +39,6 @@ Frame_main.pack(ipadx = 900, ipady =530,side = BOTTOM)
 score = get_score_list()
 print(score)
 
-BoutonS(0,1, "Tete", Tete).command()
-
-
 
 nom_de_jeux = ["Tête Cherseuse", "Pong", "space Invaders", "Snake", "Tetris", "Jeu 6", "Jeu 7", "Jeu 8",]
 
@@ -47,7 +46,7 @@ for i in range(9):
     Frame_main.rowconfigure(i, weight = 1)
     Frame_main.columnconfigure(i ,weight =1)
 
-#BoutonS(1, 1, "bite").command()
+#BoutonS(1, 1, "bite", Tete).command()
 
 Label_list0= Label(Frame_main, text = nom_de_jeux[0])
 Label_list1= Label(Frame_main, text = nom_de_jeux[1])
@@ -58,12 +57,15 @@ Label_list5= Label(Frame_main, text = nom_de_jeux[5])
 Label_list6= Label(Frame_main, text = nom_de_jeux[6])
 Label_list7= Label(Frame_main, text = nom_de_jeux[7])
 
-Label_list0.grid(row = 2, column = 2)
-Label_list1.grid(row = 2, column = 4)
-Label_list2.grid(row = 2, column = 6)
-Label_list3.grid(row = 2, column = 8)
-Label_list4.grid(row = 4, column = 2)
-Label_list5.grid(row = 4, column = 4)
-Label_list6.grid(row = 4, column = 6)
-Label_list7.grid(row = 4, column = 8)
+Label_list0.grid(row = 2, column = 1)
+Label_list1.grid(row = 2, column = 3)
+Label_list2.grid(row = 2, column = 5)
+Label_list3.grid(row = 2, column = 7)
+Label_list4.grid(row = 4, column = 1)
+Label_list5.grid(row = 4, column = 3)
+Label_list6.grid(row = 4, column = 5)
+Label_list7.grid(row = 4, column = 7)
+
+bouton_0 = BoutonS(1, 3, "Tete", Tete)
+
 root_main.mainloop()
