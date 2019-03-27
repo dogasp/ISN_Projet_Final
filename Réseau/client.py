e359b922dbdec62081a8e1bcf78e358f9edb2be8
dogasp <dorian.gaspar95@free.fr>
2019-03-27 14:06:43 +0100

    on enleve les trucs innutiles

--- Réseau/client.py
+++ Réseau/client.py
@@ -5,8 +5,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1243))
 
 while True:
-    new_msg = True
-    while True:
-        msg = s.recv(1000)
+    msg = s.recv(1000)
 
-        print(pickle.loads(msg))
+    print(pickle.loads(msg))
