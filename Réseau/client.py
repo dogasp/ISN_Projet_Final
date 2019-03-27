5075ae3f341086f7da1ff72b01c5afe7a06592b6
dogasp <dorian.gaspar95@free.fr>
2019-03-27 14:16:45 +0100

    comments

--- Réseau/client.py
+++ Réseau/client.py
@@ -1,10 +1,10 @@
-import socket
+import socket #imports
 import pickle
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1243))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #création du socket
+s.connect((socket.gethostname(), 1243)) #on lie l'adresse ip et le ports
 
 while True:
-    msg = s.recv(1000)
+    msg = s.recv(1000) #on cherche a recevoir un message
 
-    print(pickle.loads(msg))
+    print(pickle.loads(msg)) #on affiche le message après l'avoir désérialisé
--- Réseau/server.py
+++ Réseau/server.py
@@ -1,17 +1,14 @@
-import socket
-import time
+import socket #imports
 import pickle
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1243))
-s.listen(5)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #création du socket
+s.bind((socket.gethostname(), 1243)) #on lie l'adresse ip et le ports
+s.listen(5) #nombre de connections entrantes acceptées
 
-while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
+while True: #boucle
+    clientsocket, address = s.accept() #récupération des informations de connection
     print(f"Connection from {address} has been established.")
 
-    d = {1:"hi", 2: "there"}
-    msg = pickle.dumps(d)
-    print(msg)
-    clientsocket.send(msg)
+    d = {1:"hi", 2: "there"} #data a envoyer
+    msg = pickle.dumps(d) #on le sérialise
+    clientsocket.send(msg) #envoi de la data au client
