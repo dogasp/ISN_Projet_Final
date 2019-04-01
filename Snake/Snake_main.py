e2f4776c5e61cdf9f863bceda6ab6a93b467a880
gwenilapeuf <gwendalauphan@gmail.com>
2019-04-01 16:00:10 +0200

    sanke start

---
+++ Snake/Snake_main.py
@@ -0,0 +1,17 @@
+from tkinter import * #@UnusedWildImport
+from tkinter.messagebox import *
+from time import sleep
+sys.path.append('../Reseau')
+from Reseau.client import *
+sys.path.append('../Scoreboard')
+from Scoreboard.scoreboard import *
+
+class snake:
+    def __init__(self):
+        pass
+
+def Snake():
+    jeux = snake()
+
+if __name__ == "__main__":
+    Snake()
