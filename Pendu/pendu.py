fa5ed1e8676801e07d06b5a400f0bbbe40b4caaf
dogasp <dorian.gaspar95@free.fr>
2019-04-05 22:33:55 +0200

    début

--- Pendu/pendu.py
+++ Pendu/pendu.py
@@ -1,1 +1,21 @@
- 
+ from tkinter import * #@UnusedWildImport
+from tkinter.messagebox import *
+from time import sleep
+sys.path.append('../Reseau')
+from Reseau.client import *
+sys.path.append('../Scoreboard')
+from Scoreboard.scoreboard import *
+
+class pendu:
+    def __init__(self, user):
+        self.user = user
+        self.score = 0
+        with open("Pendu/ressources/liste_francais.txt") as f:
+            sefl.data = f.read().lower().split("\n")
+        
+
+
+
+def Pendu(user):
+    partie = pendu(user)
+    return partie.score
