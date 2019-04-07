11061357fe76fdb7acae6836b5a97af830dd2df2
dogasp <dorian.gaspar95@free.fr>
2019-04-08 01:13:03 +0200

    comments

--- Tete_chercheuse/tete_chercheuse.py
+++ Tete_chercheuse/tete_chercheuse.py
@@ -19,7 +19,7 @@ from Scoreboard.scoreboard import *
 #meilleur score possible: 4163,9
 ########################################################################################
 
-def rules_game():
+def rules_game(): #fonction pour afficher les regles du jeu
     global show_rules, Frame_main2_wind2, Frame_main1_wind2
     show_rules = Toplevel()
 
@@ -42,14 +42,15 @@ def rules_game():
     Rules.place(x = 200, y =5)
 
 #------------------1----------------------------------------------------------------
+    #détail d'une rêgle
     Rules2 = Label(Frame_main2_wind2, text = "Le but est que le robot arrive au drapeau.\n\
      ", font = ("Berlin Sans FB", 12))
-    Frame_main2_wind2.after(500, lambda: Rules2.place(x = 45, y = 70))
+    Frame_main2_wind2.after(500, lambda: Rules2.place(x = 45, y = 70)) #affichage après un délais
 
-    CANVAS1 = Canvas(Frame_main2_wind2, width = 150, height = 60)
-    Frame_main2_wind2.after(1000, lambda: CANVAS1.place(x = 380, y = 60 ))
-    first_photo1 = PhotoImage(file = "Rules_tete_photo/first_photo.png")
-    CANVAS1.create_image(75, 30,image = first_photo1)
+    CANVAS1 = Canvas(Frame_main2_wind2, width = 150, height = 60) #canvas pour afficher l'image
+    Frame_main2_wind2.after(1000, lambda: CANVAS1.place(x = 380, y = 60 )) #placement du canvas
+    first_photo1 = PhotoImage(file = "Rules_tete_photo/first_photo.png") #import de l'image
+    CANVAS1.create_image(75, 30,image = first_photo1) #création de l'image
 
 #------------------2-----------------------------------------------------------------
     Rules3 = Label(Frame_main2_wind2, text = 'Pour cela, tu as à disposition des caisses \n\
@@ -88,48 +89,48 @@ def rules_game():
 
     show_rules.mainloop()
 
-def quit_ranking():
+def quit_ranking(): #fonction pour quitter la fenetre des rêgles et du classement
     show_rules.destroy()
     show_rules.quit()
 
-def quit_rules():
+def quit_rules(): #fonction pour quiter les rêgles et aller sur les classements
     Frame_main2_wind2.destroy()
-    score = Scoreboard(Frame_main1_wind2, show_rules, "Tete", User_name)
+    Scoreboard(Frame_main1_wind2, show_rules, "Tete", User_name) #appel de la classe du scoreboard
 
-def warning():
+def warning(): #warning appelé au moment du clique sur le bouton skip au niveau des rêgles
     showwarning("Attention", "Attention !! La partie commencera quand tu \n appuyeras sur le bouton 'Continuer' \n et le temps est compté donc prépare-toi.")
     quit_rules()
 
 """######################------------------Début du Jeu---------------------------########################################"""
 
-def click(event):
+def click(event): #fonction appelée lors du clique de la souris sur le canvas
     global box_placed
-    x = event.x
+    x = event.x #on récupère la position de la souris sur l'écran
     y = event.y
 
-    i = int(x//cell_width)
+    i = int(x//cell_width) #on convertit la position de la souris en indice pour le tableau
     j = int(y//cell_height)
 
-    if table[i][j] == "0":
-        table[i][j] = "C"
-        box_placed+=1
+    if table[i][j] == "0":   # si on se trouve sur une case vide
+        table[i][j] = "C"    # on ajoute une caisse
+        box_placed+=1        # et on incrémente le nombre de caisses en actualisant l'affichage
         show_count['text'] = "Nombre de palettes: %s" %str(box_placed)
-    elif table[i][j] == "C":
-        table[i][j] = "0"
-        box_placed-=1
+    elif table[i][j] == "C": # si on clique sur une caisse
+        table[i][j] = "0"    # on l'enlève
+        box_placed-=1        # on décrémente le compteur en actualisant l'affichage
         show_count['text'] = "Nombre de palettes: %s" %str(box_placed)
     update()
 
-def update(Print_Score = True):
-    Table.delete("all")
-    for i in range(nbcases_width):
-        Table.create_line((cell_width)*i,0,(cell_width)*i,500)
+def update(Print_Score = True): #fonction pour regénérer l'affichage
+    Table.delete("all") #on supprime tout ce qu'il y a sur le canvas
 
+    for i in range(nbcases_width): #recréation des lignes
+        Table.create_line((cell_width)*i,0,(cell_width)*i,500)
     for j in range(nbcases_height):
         Table.create_line(0,(cell_width)*j,500,(cell_width)*j)
 
-    Table.create_rectangle(2,2,500,500)
-    for i in range(10):
+    Table.create_rectangle(2,2,500,500) #contours
+    for i in range(10): #pour chaques cases, on regarde ce qu'il y a dans le tableau et on crée l'affichage correspondant
         for j in range(10):
             if (table[i][j])=='X':
                 Table.create_image(cell_width* i + cell_width/2, cell_height* j + cell_height/2, image = Wall)
@@ -147,109 +148,111 @@ def update(Print_Score = True):
                 Table.create_image(cell_width* i + cell_width/2, cell_height* j + cell_height/2, image = End)
     root_tete.update()
 
-    if Print_Score == True:
-        show_score["text"] = "Score: %s" %str(int(sum(score)))
+    if Print_Score == True: #on peut appeler la fonction update sans actualiser le score
+        show_score["text"] = "Score: %s" %str(int(sum(score))) #actualisation du score
 
-def end_game():
+def end_game(): #fonction appelée quand la partie se termine
     global question, box_placed, score_temp
-    score_temp = (10000/(box_placed*10 + time_game*0.2) + score_star) * level
-    show_score["text"] = "Score: %s"%str(int(sum(score + [score_temp])))
-    Button_restart["state"] = "disabled"
+    score_temp = (10000/(box_placed*10 + time_game*0.2) + score_star) * level #calcul du score
+    show_score["text"] = "Score: %s"%str(int(sum(score + [score_temp]))) #on actualise l'affichage
+    Button_restart["state"] = "disabled" #désactivation du bouton restart
     update(False)
 
     question = Toplevel()
     question.geometry("300x125")
-    question.protocol("WM_DELETE_WINDOW", exit_menu)
+    question.protocol("WM_DELETE_WINDOW", exit_menu) #on demande les actions a faire
     Button(question, text = "Restart", command = restart_question,cursor ='hand2', font = ("Helvetica", 10)).place(x = 30, y = 45)
     Button(question, text = "Main Menu", command = exit_menu,cursor ='hand2', font = ("Helvetica", 10)).place(x = 210, y = 45)
     Button(question, text = "Next Level", command = next,cursor ='hand2', font = ("Helvetica", 10)).place(x = 110, y = 45)
 
-def restart_question():
+def restart_question(): # fonction executée si le joueur veut recommencer le niveau
     global question2, question
     question.destroy()
     question2 = askquestion("RESTART", "Est-tu-sur de recommencer ? Tu perdras à chaque fois 50 points multiplié par le niveau où tu es")
     if question2 == "yes": #si l'utilisateur veut recommencer, on regenère l'affichage
         restart()
-    else:
+    else: #sinon on redemande l'action a executer
         end_game()
 
-box_placed = 0
-time_game = 0
+box_placed = 0 #nombre de box que l'utilisateur a posé sur la map
+time_game = 0 #temps pris par le joueur pour résoudre l'agnime
 
-def time_num():
+def time_num(): #fonciton pour incrémenter le temps
     global time_game
-    time_game+=1
-    show_time['text'] = "Temps: %s" %str(time_game)
-    root_tete.after(1000,time_num)
+    time_game+=1 # incrémentation de la variable
+    show_time['text'] = "Temps: %s" %str(time_game) #actualisation de l'affichage
+    root_tete.after(1000,time_num) #rappel de la fonction après 1 seconde (1000 ms)
 
-def next():
+def next(): #fonction pour passer au niveau suivant
     global level, score
-    score[-1] += score_temp
-    score.append(50*(level+1))
+    score[-1] += score_temp     # on atribue au dernier niveau joué le score gardé en mémoire
+    score.append(50*(level+1))  # création d'un nouveau slot dans la liste des scores pour le nouveau niveau initié avec une valeur car il y a un malus appliqué dans la foction restart
     question.destroy()
-    level += 1
-    if level == len(Levels)+1:
-        exit()
+    level += 1                  # incrémentation du niveau
+    if level == len(Levels)+1:  # si le joueur a atteint la fin de la liste des niveaux
+        score[-1] = 0           # le dernier score ne doit pas avoir d'offset
+        exit()                  # on quitte le jeu en arretant la fonction
         return
-    Title_level["text"] = f"Level {level}"
-    restart()
+    Title_level["text"] = "Level {}".format(level) #actualisation de l'affichage
+    restart()                   # régénération de l'affichage
 
-def start():
+def start(): #fonction pour faire bouger le robot
     global table, index_robot, score_star, run
-    Table.unbind("<Button-1>")
+    Table.unbind("<Button-1>")          # on désactive le clique et le bouton start
     Button_start["state"] = "disabled"
-    dir = [1, 0] #matrice de mouvement
+    dir = [1, 0]                        # matrice de mouvement, par défaut, on va a droite
     for i in range(nbcases_width):
-        for j in range(nbcases_height):
+        for j in range(nbcases_height): # on récupère la position du robot en parcourant la liste
             if table[i][j] == "R":
                 pos = [i, j]
                 break
-    reminder = {} #pour vérifier le nombre de fois qu'on est passé a un meme endroit
-    run = True
+    reminder = {} # pour vérifier le nombre de fois qu'on est passé a un meme endroit
+    run = True    # variable vraie tant qu'on execute la boucle
     while run == True:
-        sleep(0.5)
-        table[pos[0]][pos[1]] = "0"
-        x = pos[0] + dir[0]
+        sleep(0.5) #on fait avancer le robot toutes les 0.5 secondes
+        table[pos[0]][pos[1]] = "0" # on remplace la case où il était par une case vide
+        x = pos[0] + dir[0] #calcul de la prochaine position
         y = pos[1] - dir[1]
-        if nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "0":
-            pos = [x, y]
+        if nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "0": #si la prochaine position est dans le tableau et que la prochaine case est libre
+            pos = [x, y] #on lui assigne la nouvelle position
             try:
-                reminder[(pos[0], pos[1])] += 1
-                if reminder[(pos[0], pos[1])] > 4: #si on est passé plus de 4 fois au meme endroit, on restart
+                reminder[(pos[0], pos[1])] += 1     # on essaye d'ajouter 1 à la position actuelle
+                if reminder[(pos[0], pos[1])] > 4:  # si on est passé plus de 4 fois au meme endroit, on restart
                     run = False
                     restart()
                     return
             except:
                 reminder[(pos[0], pos[1])] = 1 #si impossible de ajouter 1 c'est que la clef n'est pas crée, on l'initialise a 1
 
-        elif nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "S":
-            pos = [x, y]
-            score_star += 50
-            table[x][y] = "0"
+        elif nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "S": #si la prochaine case est une petite pièce
+            pos = [x, y]        # on lui atribue la nouvelle position
+            score_star += 50    # ajout du bonnus
+            table[x][y] = "0"   # et on vide la case
 
-        elif nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "B":
+        elif nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "B": #même chose si la prochaine case est une grosse pièce
             pos = [x, y]
             score_star += 150
             table[x][y] = "0"
 
 
-        elif nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "P":
-            table[x][y] = "E"
-            run = False
-            end_game()
-            return
-        else:
-            dir = [dir[1], -dir[0]] #rotation d'une matrice [a, b] par -PI/2 en faisant [b, -a]
-            index_robot = (index_robot + 1)%4
-        table[pos[0]][pos[1]] = "R"
+        elif nbcases_width > x >= 0 and nbcases_height > y >= 0 and table[x][y] == "P": #si la prochaine case est le drapeau
+            table[x][y] = "E" #on change la case actuelle à réussi pour afficher avec le drapeau
+            run = False #on arrete la boucle
+            end_game()  #fin du jeu
+            return      # fin de la fonction
+
+        else:                                   # sinon, on a un obstacle devant le robot
+            dir = [dir[1], -dir[0]]             # rotation d'une matrice [a, b] par -PI/2 en faisant [b, -a]
+            index_robot = (index_robot + 1)%4   # affichage de la rotation
+        table[pos[0]][pos[1]] = "R"             # on place la robot dans la grille à sa position actuelle
         update()
 
-def restart_button():
+def restart_button(): #si on clique sur le bouton pour restart
     global run, score_star
-    run = False
-    restart()
+    run = False #on arrete la boucle
+    restart()   #restart de l'application
 
-def restart():
+def restart(): #fonction appelée pour redémarer
     global table, timer_start, time_game, box_placed, index_robot, score_star, score
     Button_restart["state"] = "normal"
     index_robot = 0
