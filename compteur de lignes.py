6a0afca7a43f055510fc65f687de0f8b9b9304b3
dogasp <dorian.gaspar95@free.fr>
2019-05-04 23:23:17 +0200

    optimizing

--- compteur de lignes.py
+++ compteur de lignes.py
@@ -1,29 +1,25 @@
 from os import listdir
-from os.path import isfile, join
-path = "./"
-fichiers = listdir(path)
+from os.path import isfile, join, isdir
+root_path = "./"
+root = listdir(root_path)
 
-lignes = 0
 total = 0
-for file in fichiers:
-    if isfile(join(path, file)):
-        if file.split(".")[1] == "py" and file != "compteur de lignes.py":
-            with open(join(path,file), "r") as fichier:
-                temp = fichier.readlines()
-                for i in temp:
-                    lignes += 1
-                print(file,  lignes)
-                total += lignes
-                lignes = 0
-    else:
-        for under in listdir(join(path,file)):
-            if len(under.split(".")) > 1 and under.split(".")[1] == "py":
-                with open(join(path,file,under), "r") as fichier:
+def counter(fichiers, path):
+    global total
+    for file in fichiers:
+        lignes = 0
+        if isfile(join(path, file)):
+            if file.split(".")[1] == "py" and file != "compteur de lignes.py":
+                with open(join(path,file), "r") as fichier:
                     temp = fichier.readlines()
                     for i in temp:
                         lignes += 1
-                    print(under, lignes)
+                    print(file,  lignes)
                     total += lignes
                     lignes = 0
+        elif isdir(join(path, file)) and file != ".git":
+            temp = join(path, file)
+            counter(listdir(temp), temp)
+counter(root, root_path)
 print("total de lignes = ", total)
 input()
