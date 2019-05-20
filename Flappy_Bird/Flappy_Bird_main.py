80bb33f89691ffe8b01cb8bdd334d9ce13c103ef
gwenilapeuf <gwendalauphan@gmail.com>
2019-05-20 17:53:01 +0200

    sdf

--- Flappy_Bird/Flappy_Bird_main.py
+++ Flappy_Bird/Flappy_Bird_main.py
@@ -21,53 +21,28 @@ class bird:
 
         self.Frame_main1_wind2 = Canvas(self.show_rules, bg = 'red', relief = GROOVE)
         self.Frame_main1_wind2.pack(ipadx = 670, ipady = 526)
-        #self.Fond_Frame_main1_wind2 = PhotoImage(file = "thumbnail/Ghost2.png")
-        #self.Frame_main1_wind2.create_image(335,263,image =self.Fond_Frame_main1_wind2)
+        self.Fond_Frame_main1_wind2 = PhotoImage(file = "thumbnail/Flappy2.png")
+        self.Frame_main1_wind2.create_image(335,263,image =self.Fond_Frame_main1_wind2)
         self.Frame_main2_wind2 = Frame(self.Frame_main1_wind2,width = 550, height = 425, relief = GROOVE)
         self.Frame_main2_wind2.place(x = 60, y = 45)
         self.Rules = Label(self.Frame_main2_wind2, text = 'Les règles:', font = ("Berlin Sans FB", 23), relief = GROOVE)
         self.Rules.place(x = 200, y =5)
 
-        self.Jerry_1 = PhotoImage(file = "Fantome/Ressources/Images/Jerry_1.png")
-        self.keyboard_fantome = PhotoImage(file = "Fantome/Ressources/Images/keyboard_fantome.png")
-        self.Jerry_3 = PhotoImage(file = "Fantome/Ressources/Images/Jerry_3.png")
-        self.Jerry_2 = PhotoImage(file = "Fantome/Ressources/Images/Jerry_2.png")
+        Label(self.Frame_main2_wind2, text = "Le but du jeu est de faire avancer l'oiseau\n entre des tuyaux").place(x = 20, y = 80)
+        self.image1 = PhotoImage(file = "Flappy_Bird/Ressources/rules1.png")
+        Label(self.Frame_main2_wind2, image = self.image1).place(x = 400, y = 60)
 
-        #détail des rêgles
+        Label(self.Frame_main2_wind2, text = "Pour ce faire, tu peux utiliser la bar espace\n ou le clic souris\n pour que l'oiseau fasse un bond").place(x = 20, y = 150)
 
-        self.Rules2 = Label(self.Frame_main2_wind2, text = "Le but est que Jerry puisse arriver \n\
-        au fromage sans que Tom l'attrape. \n Pour cela tu pourras utiliser les flèches \n du keyboard afin de déplacer Jerry", font = ("Berlin Sans FB", 12))
-        self.Frame_main2_wind2.after(500, lambda: self.Rules2.place(x = 40, y = 70))
+        Label(self.Frame_main2_wind2, text = "Mais attention, si tu touche un tuyau ou le sol,\n l'oiseau meurt").place(x = 20, y = 220)
+        self.image3 = PhotoImage(file = "Flappy_Bird/Ressources/rules2.png")
+        Label(self.Frame_main2_wind2, image = self.image3).place(x = 400, y = 200)
 
-        self.CANVAS1 = Canvas(self.Frame_main2_wind2, width = 120, height = 70)
-        self.CANVAS2 = Canvas(self.Frame_main2_wind2, width = 100, height = 70)
-        self.Frame_main2_wind2.after(1000, lambda: self.CANVAS1.place(x = 388, y = 35))
-        self.Frame_main2_wind2.after(1000, lambda: self.CANVAS2.place(x = 400, y = 106))
-        self.CANVAS1.create_image(60, 35,image = self.Jerry_1)
-        self.CANVAS2.create_image(50, 35,image = self.keyboard_fantome)
-        self.CANVAS2.create_rectangle(2,2,98,68, outline='black')
+        self.image4 = PhotoImage(file = "Flappy_Bird/Ressources/rules3.png")
+        Label(self.Frame_main2_wind2, image = self.image4).place(x = 350, y = 300)
 
-    #------------------2-----------------------------------------------------------------
-        self.Rules3 = Label(self.Frame_main2_wind2, text = "Mais Attention !! Tom va plus vite que toi car il peut \n\
-        se déplacer en diagonale. Tom se déplace par \n rapport à Jerry et fait tout pour se rapprocher." ,font = ("Berlin Sans FB", 12))
-        self.Frame_main2_wind2.after(2000, lambda: self.Rules3.place(x = 30, y = 202))
-
-        self.CANVAS3 = Canvas(self.Frame_main2_wind2,  width = 100, height = 100)
-        self.Frame_main2_wind2.after(2500, lambda: self.CANVAS3.place(x = 400, y = 183 ))
-        self.CANVAS3.create_image(50,50, image = self.Jerry_3)
-
-    #------------------3------------------------------------------------------------------
-        self.Rules4 = Label(self.Frame_main2_wind2, text = "L'astuce est alors de bloquer le robot grâce\n\
-        aux bloques disposés sur la carte",font = ("Berlin Sans FB", 12))
-        self.Frame_main2_wind2.after(3500, lambda: self.Rules4.place(x = 40, y = 315))
-
-        self.CANVAS4 = Canvas(self.Frame_main2_wind2, width = 130, height = 95)
-        self.Frame_main2_wind2.after(4000, lambda: self.CANVAS4.place(x = 388, y = 293 ))
-        self.CANVAS4.create_image(65, 47,image = self.Jerry_2)
-
-    #------------------Skip------------------------------------------------------------------
         self.Button_Skip = Button(self.Frame_main2_wind2, text = "-Skip-", cursor ='hand2', command = self.quit_rules)
-        self.Button_Skip.place(x = 200, y = 390)
+        self.Button_Skip.place(x = 50, y = 370)
         self.show_rules.mainloop()
 
         """#################----------------- début du jeu ----------------#################### """
@@ -94,6 +69,8 @@ class bird:
         self.tap = [PhotoImage(file = 'Flappy_Bird/Ressources/tap_right.png'), PhotoImage(file = 'Flappy_Bird/Ressources/tap_left.png')]
         self.hand_image = PhotoImage(file = 'Flappy_Bird/Ressources/hand.png')
         self.image = PhotoImage(file = "Flappy_Bird/Ressources/game_over.png")
+        self.title =  PhotoImage(file = 'Flappy_Bird/Ressources/title.png')
+        self.fond_decor = PhotoImage(file = 'Flappy_Bird/Ressources/fond_decor.png')
         self.build_game()
         self.root.mainloop()
 
@@ -126,36 +103,30 @@ class bird:
         self.root.bind("<Button-1>", self.test_press)
 
         ########-----------Frames Principaux------------#######################################
-        self.Frame_left = Frame(self.root, width = 200, height = 570, bg = 'white')
-        self.Frame_top = Frame(self.root, width = 900, height = 50, bg = 'grey')
-        self.Frame_right = Frame(self.root, width = 700, height = 570, bg = 'black')
+        self.Frame_left = Canvas(self.root, width = 900, height = 620, bg = 'white', highlightthickness=0)
+        self.Frame_right = Frame(self.Frame_left, width = 700, height = 570, bg = 'black')
 
-        ########-----------Frames Secondaires------------#######################################
-        self.Frame1 = Frame(self.Frame_left, width = 200, height = 200, bg = 'yellow')
-        self.Frame2 = Frame(self.Frame_left, width = 200, height = 300, bg = 'red')
-
-        #######-----------Package des Frames-------------####################################
-        self.Frame_top.pack(side = TOP)
-        self.Frame_left.pack(side = LEFT)
-        self.Frame1.pack(side = TOP)
-        self.Frame2.pack(side = BOTTOM)
-        self.Frame_right.pack(side = RIGHT)
-
+        self.Frame_left.place(x = 0, y = 0)
+        self.Frame_right.place(x = 150, y = 60)
+        self.Frame_left.create_image(450,310, image = self.fond_decor)
 
         ######-----------Elements du jeu-----------------##########################################
-        self.sentence = Label(self.Frame2, text = "Attrape Moi \n Si Tu Peux !!!",font = ("Berlin Sans FB", 15), bg = 'white')
-        self.sentence.place(x = 45, y = 60)
+        self.Frame_left.create_text(70,360 ,text = "Attention aux tuyaux !!!! ",fill  = 'white',font = ("Berlin Sans FB", 14))
+        self.Frame_left.create_image(70,400 ,image = self.liste_image[5])
+        self.Frame_left.create_text(70,460 ,text = "Tu as des bonus de \n points tous les 10 tuyaux",fill  = 'white' ,font = ("Berlin Sans FB", 12))
+        bestplayer = get_game_score_list("Flappy")[0]
+        self.Frame_left.create_text(70,100 ,text = "Meilleur joueur:" ,fill  = 'white', font = ("Berlin Sans FB", 14))
+        self.Frame_left.create_text(70,115 ,text = "{} points".format(int(bestplayer[1])),fill  = 'white', font = ("Berlin Sans FB", 14))
 
-        self.canvas_show_time = Canvas(self.Frame1, bg = 'black',highlightthickness=0)
+
+        self.canvas_show_time = Canvas(self.Frame_left, bg = 'black',highlightthickness=0)
         self.canvas_show_time.place(x = 10, y = 30)
 
         self.show_time = Label(self.canvas_show_time, text = "Temps: %s" %str(0), foreground = 'blue2', bg = 'black',font = ("Berlin Sans FB", 24))
         self.show_time.pack(padx= 3, pady = 3)
 
-        bestplayer = get_game_score_list("Flappy")[0]
-        self.best_label = Label(self.Frame_top, text = "Meilleur joueur:\n{} avec {} points".format(bestplayer[0], int(bestplayer[1])), bg = "black", fg = "#00cc00") #je te laisse la mise en forme
-        self.best_label.place(x = 30, y = 15)
 
+        self.Frame_left.create_image(450,32, image = self.title)
 
         #######---------------Variables du jeu-------------###########################
         self.x = 0                      #Déplacement du bird en x
@@ -344,8 +315,6 @@ class bird:
                         self.verif_bird(0,0,b = True)                                                       # Même résonnement
                     else:                                                                                   #
                         self.Canvas_world.move(self.image_Bird_true, self.x,self.vitesse)                   ############################
-            print(self.y_center_bird)
-
             self.tuyau0.move_pipe()
             self.tuyau1.move_pipe()
             self.tuyau2.move_pipe()
@@ -401,7 +370,6 @@ class bird:
         if self.question == "yes":                        # si l'utilisateur veut recommencer, on regenère l'affichage
             self.Frame_right.destroy()               # destruction des frames
             self.Frame_left.destroy()                #
-            self.Frame_top.destroy()                 #
             self.build_game()                        # reconstruction de la fenètre
         else:
             self.exit()                              #sinon, on quitte l'application
