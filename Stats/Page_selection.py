0a7df1ca96cd506115d89ba4aff057a6f3b95e72
gwenilapeuf <gwendalauphan@gmail.com>
2019-06-05 01:04:57 +0200

    sdf

--- Stats/Page_selection.py
+++ Stats/Page_selection.py
@@ -11,51 +11,90 @@ class Stats:
         self.root.geometry("1000x600")
         self.root.protocol()
 
-        self.Gros_Titre_Sa_Mere = Label(self.root, text = "Page de sélection des graphs à afficher")
-        self.Gros_Titre_Sa_Mere.place(x = 200, y = 20)
+        self.titre = Label(self.root, text = "Page de sélection des graphs à afficher")
+        self.titre.place(x = 200, y = 20)
 
         self.games = get_player_score("dodo").keys()
         self.game_ToSend = "Flappy"
-      
+
         self.listbox_1 = Listbox(self.root)
-        self.listbox_1.place(x = 400, y = 60)
+        self.listbox_1.place(x = 50, y = 60)
         self.listbox_1.insert(END, "Statistiques sur toi")
         self.listbox_1.insert(END, "Statistiques globales")
-        self.listbox_1.bind("<ButtonRelease-1>", self.Select_mode)
+        self.listbox_1.bind("<ButtonRelease-1>", self.get_variable_1)
         self.selected_mode = 0
-#------------------------------------------
-        self.listbox_2 = Listbox(self.root)
-        self.listbox_2.place(x = 150, y = 60)
-        self.listbox_2.insert(END, "tout les jeux")
-        for elt in self.games:
-            self.listbox_2.insert(END, elt)
-        self.listbox_2.bind("<ButtonRelease-1>", self.Select_game)
 
-#-----------------------------------------
+        self.listbox_2 = Listbox(self.root)
+        self.listbox_2.place(x = 200, y = 60)
+        self.listbox_2.insert(END, "Statistiques sur Application")
+        self.listbox_2.insert(END, "Statistiques sur jeu")
+        self.listbox_2.bind("<ButtonRelease-1>", self.Select_mode)
+        self.selected_mode2 = 0
+
         validate_button = Button(self.root, text = "validate", command = self.launch)
         validate_button.place(x = 500, y = 300)
 
         self.root.mainloop()
 
-    def Select_game(self, event = None):
-        a  =self.listbox_2.curselection()
-        self.game_ToSend = self.listbox_2.get(a)
+    def get_variable_1(self, event = None):
+        a = self.listbox_1.curselection()
+        print(a)
+        self.selected_mode = self.listbox_1.get(a)
+        print(self.selected_mode)
 
     def Select_mode(self, event = None):
-        a = self.listbox_1.curselection()
-        self.selected_mode = self.listbox_1.get(a)
+        a  = self.listbox_2.curselection()
+        self.selected_mode2 = self.listbox_2.get(a)
 
-        self.Encore_Une_Autre_Belle_Liste = Listbox(self.root)
-        self.Encore_Une_Autre_Belle_Liste.place(x = 500, y = 60)
-        self.Encore_Une_Autre_Belle_Liste.insert(END, "Application")
-        self.Encore_Une_Autre_Belle_Liste.insert(END, "Thermique")
-        self.Encore_Une_Autre_Belle_Liste.bind("<ButtonRelease-1>", self.Select_graphType)
-        self.selected_mode = "Application"
+        if self.selected_mode2 == "Statistiques sur Application":
+            self.listbox_3 = Listbox(self.root)
+            self.listbox_3.place(x = 350, y = 60)
+            self.listbox_3.insert(END, "stat1")
+            self.listbox_3.insert(END, "stat2")
+            self.listbox_3.insert(END, "stat3")
+            self.listbox_3.insert(END, "stat4")
+            self.listbox_3.bind("<ButtonRelease-1>", self.get_variable_3)
+        else:
+            self.listbox_3_bis = Listbox(self.root)
+            self.listbox_3_bis.place(x = 350, y = 60)
+            self.listbox_3_bis.insert(END, "tout les jeux")
+            for elt in self.games:
+                self.listbox_3_bis.insert(END, elt)
+            self.listbox_3_bis.bind("<ButtonRelease-1>", self.get_variable_2)
+
+    def get_variable_2(self, event = None):
+        a = self.listbox_3_bis.curselection()
+        self.selected_mode3 = self.listbox_3_bis.get(a)
+        self.Select_graphType()
+
+    def get_variable_3(self, event = None):
+        a = self.listbox_3.curselection()
+        self.selected_mode4 = self.listbox_3.get(a)
+        self.Select_graphType()
+
 
     def Select_graphType(self, event = None):
-        a = self.Encore_Une_Autre_Belle_Liste.curselection()
-        self.type_stats = self.Encore_Une_Autre_Belle_Liste.get(a)
-    
+        self.listbox_4 = Listbox(self.root)
+        self.listbox_4.place(x = 500, y = 60)
+        self.listbox_4.insert(END, "Application")
+        if self.selected_mode == "Statistiques sur jeu":
+            pass
+            """ A poursuivre...."""
+
+
+        self.listbox_4.insert(END, "Thermique")
+        self.listbox_4.bind("<ButtonRelease-1>", self.Select_graphType2)
+
+
+    def Select_game(self, event = None):
+        a  = self.listbox_3.curselection()
+        self.game_ToSend = self.listbox_3.get(a)
+
+
+    def Select_graphType2(self, event = None):
+        a = self.listbox_4.curselection()
+        self.type_stats = self.listbox_4.get(a)
+
     def launch(self):
         print(self.game_ToSend, self.selected_mode)
 
@@ -71,7 +110,7 @@ class Stats:
                 for game in data.keys():
                     moyennes[game] = data[game]["moyenne"][0]
                     total[game] = data[game]["moyenne"][1]
-                                               
+
             else:
                 if self.game_ToSend != "tout les jeux": #si c'est un jeu spécifique
                     x = []
@@ -92,4 +131,3 @@ class Stats:
 
     def show_selected_grap(self):
         pass
-
