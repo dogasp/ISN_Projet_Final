4039123ebc9f3bcb5a54d5a3dcc66bfe99a8730d
gwenilapeuf <gwendalauphan@gmail.com>
2019-06-04 21:08:44 +0200

    j
    Co-authored-by: dogasp <dorian.gaspar95@free.fr>

--- Stats/Page_selection.py
+++ Stats/Page_selection.py
@@ -13,6 +13,34 @@ class Stats:
         self.Gros_Titre_Sa_Mere = Label(self.root, text = "Page de sélection des graphs à afficher")
         self.Gros_Titre_Sa_Mere.place(x = 200, y = 20)
 
-        app = Graph_3(self.root)
+        self.games = get_player_score("dodo").keys()
+        self.game_ToSend = self.games[0]
+
+        self.Une_Belle_Liste = Listbox(self.root)
+        self.Une_Belle_Liste.place(x = 150, y = 60)
+        for elt in self.games:
+            self.Une_Belle_Liste.insert(END, elt)
+        self.Une_Belle_Liste.bind("<ButtonRelease-1>", self.Select_game)
+
+        self.Une_Autre_Belle_Liste = Listbox(self.root)
+        self.Une_Autre_Belle_Liste.place(x = 400, y = 60)
+        self.Une_Autre_Belle_Liste.insert(END, "Statistiques sur toi")
+        self.Une_Autre_Belle_Liste.insert(END, "Statistiques globales")
+        self.Une_Autre_Belle_Liste.bind("<ButtonRelease-1>", self.Select_mode)
+        self.selected_mode = 0
+
+        validate_button = Button(self.root, text = "validate", command = self.launch)
+
+        #app = Graph_3(self.root)
 
         self.root.mainloop()
+
+    def Select_game(self, event = None):
+        a  =self.Une_Belle_Liste.curselection()
+        self.game_ToSend = self.Une_Belle_Liste.get(a)
+
+    def Select_mode(self, event = None):
+        self.selected_mode = self.Une_Autre_Belle_Liste.curselection()
+    
+    def launch(self):
+        print(self.game_ToSend, self.Select_mode)
