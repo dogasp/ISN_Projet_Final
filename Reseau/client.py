08d85a3a7be343a60fe4c17db81768a36c88530d
dogasp <dorian.gaspar95@free.fr>
2019-06-20 21:44:08 +0200

    fixed a fucking bugg

--- Reseau/client.py
+++ Reseau/client.py
@@ -71,7 +71,7 @@ def get_statistics():
         "Tetris": {"moyenne":[0, 0], "player_count":{}}, "Pendu": {"moyenne":[0, 0], "player_count":{}}, "Pong": {"moyenne":[0, 0], "player_count": {}},\
         "Space": {"moyenne":[0, 0], "player_count":{}}, "Flappy": {"moyenne":[0, 0], "player_count":{}} }, {(0,0):0}]
     s.send("statistics_get ".encode("utf-8")) #on demande la liste
-    response = s.recv(1024)
+    response = s.recv(2048)
     response = pickle.loads(response) #on désérialise la réponse pour récupérer un dictionnaire
 
     s.close()
