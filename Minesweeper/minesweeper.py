51c1ef11b5e4c3979ce45b28d274fa3901b99088
dogasp <dorian.gaspar95@free.fr>
2019-06-27 14:56:56 +0200

    server bugg

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -283,4 +283,4 @@ class demineur:
 
 def Minesweeper(user):    # fonction appelée pour lancer le jeu
     jeux = demineur(user) # création de la classe
-    (jeux.score, sum(jeux.average_score)/jeux.count, (time()-jeux.time_start)/jeux.count, jeux.count, jeux.death_pos)   #renvois les données
+    return (jeux.score, sum(jeux.average_score)/jeux.count, (time()-jeux.time_start)/jeux.count, jeux.count, jeux.death_pos)   #renvois les données
